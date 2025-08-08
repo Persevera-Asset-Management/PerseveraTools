07a38dae98d361f24def4a3d3bc08fd7c033e3ca
Thales Carmo <thales.carmo@persevera.com.br>
2025-08-08 11:44:43 -0300

    Update version to 0.9.7 and add Google Sheets credentials loading in settings
    - Bump version number to 0.9.7 in __init__.py.
    - Introduce Google Sheets API credentials loading in settings.py for enhanced integration.
    - Add method to retrieve Google Sheets client secret configuration.

--- persevera_tools/config/settings.py
+++ persevera_tools/config/settings.py
@@ -37,9 +37,15 @@ class Settings:
         self.FIBERY_API_TOKEN = os.getenv('PERSEVERA_FIBERY_API_TOKEN')
         self.FIBERY_DOMAIN = os.getenv('PERSEVERA_FIBERY_DOMAIN')
 
+        # Load Google Sheets credentials
+        self.GS_CLIENT_ID = os.getenv('PERSEVERA_GOOGLESHEETS_CLIENT_ID')
+        self.GS_CLIENT_SECRET = os.getenv('PERSEVERA_GOOGLESHEETS_CLIENT_SECRET')
+        self.GS_PROJECT_ID = os.getenv('PERSEVERA_GOOGLESHEETS_PROJECT_ID')
+        self.GS_API_KEY = os.getenv('PERSEVERA_GOOGLESHEETS_API_KEY')
+
         # Load logging configuration with environment variable overrides
         self.LOG_CONFIG = DEFAULT_CONFIG['LOG_CONFIG'].copy()
-        
+
         # Override logging settings from environment variables if provided
         if os.getenv('PERSEVERA_LOG_FORMAT'):
             self.LOG_CONFIG['log_format'] = os.getenv('PERSEVERA_LOG_FORMAT')
@@ -49,6 +55,21 @@ class Settings:
         
         if os.getenv('PERSEVERA_LOG_LEVEL'):
             self.LOG_CONFIG['default_level'] = os.getenv('PERSEVERA_LOG_LEVEL')
+
+    def get_gs_client_secret(self):
+        return {
+            "installed":{
+                "client_id":self.GS_CLIENT_ID,
+                "project_id":self.GS_PROJECT_ID,
+                "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+                "token_uri":"https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+                "client_secret":self.GS_CLIENT_SECRET,
+                "redirect_uris":[
+                    "http://localhost"
+                ]
+            }
+        }
 
     def validate(self) -> bool:
         """Validate that all required configuration is present"""
