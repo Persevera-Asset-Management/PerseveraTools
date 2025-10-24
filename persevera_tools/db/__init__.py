80d23335f9bf5e7149467d2d335956616ed975ef
Thales Carmo <thales.carmo@persevera.com.br>
2025-10-24 19:21:32 -0300

    Update version to 0.9.29 in __init__.py and remove read_fibery from the public API in db module

--- persevera_tools/db/__init__.py
+++ persevera_tools/db/__init__.py
@@ -1,10 +1,8 @@
 from .connection import get_db_engine
 from .operations import to_sql, read_sql
-from .fibery import read_fibery
 
 __all__ = [
     "get_db_engine",
     "to_sql",
     "read_sql",
-    "read_fibery"
 ]
