dbecd2a56b3a162b08ff7aa22b5474a0ffb3b28d
Thales Carmo <thales.carmo@persevera.com.br>
2025-11-07 20:34:10 -0300

    Update version to 0.9.34 in __init__.py and remove XPWSProvider from data providers list.

--- persevera_tools/data/providers/__init__.py
+++ persevera_tools/data/providers/__init__.py
@@ -14,7 +14,6 @@ from .investing_com import InvestingComProvider
 from .debentures_com import DebenturesComProvider
 from .mdic import MDICProvider
 from .b3 import B3Provider
-from .xp_ws import XPWSProvider
 
 __all__ = [
     # Base classes
@@ -24,5 +23,5 @@ __all__ = [
     'BloombergProvider', 'SGSProvider', 'FredProvider', 'SidraProvider', 
     'AnbimaProvider', 'SimplifyProvider', 'CVMProvider', 'InvescoProvider',
     'ComdinheiroProvider', 'BcbFocusProvider', 'KraneSharesProvider', 'InvestingComProvider',
-    'DebenturesComProvider', 'MDICProvider', 'B3Provider', 'XPWSProvider'
+    'DebenturesComProvider', 'MDICProvider', 'B3Provider'
 ]
